3f4bc31b605ec67593b96f1e17a23596c002a9a5
wanjugucode <karobiaanastasia>
2025-04-18 13:47:18 +0300

    updates

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+  
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+      hamburger.classList.toggle('open');
+    });
+  });
+  
